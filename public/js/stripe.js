18614d217d943db1d11c7c979646241f44ac9670
yousef aref <yousefmostafa222004@gmail.com>
2024-09-25 23:13:54 +0300

    urls changed to relative

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -3,7 +3,7 @@ import { showAlert } from './alerts';
 const stripe = Stripe('pk_test_51Q2GQdP2dqphUs2ANZcH9ykueQWJjhNyWiKOAJEcoFLCsIQGYZtVeZtoBtliFU9DKtxavocoPQDtxG3NxL3ouXt100jw2yMDyL')
 
 export const bookTour = async tourId=>{
-	const session = await axios(`http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`);
+	const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
 	
 	if(session.status === 200){
 		await stripe.redirectToCheckout({
